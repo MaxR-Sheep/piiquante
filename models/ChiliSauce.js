25cc57e5b0eba46074099605281243c8466226f2
Maxime Ronjat <maxime.ronjat@gmail.com>
2023-03-30 18:55:07 +0200

    modification schéma ChiliSauce

--- models/ChiliSauce.js
+++ models/ChiliSauce.js
@@ -1,17 +1,18 @@
 const mongoose = require("mongoose");
 
+//Schéma pour les sauces piqauntes
 const chiliSauceSchema = mongoose.Schema({
   userId: { type: String, required: true },
   name: { type: String, required: true },
   manufacturer: { type: String, required: true },
   description: { type: String, required: true },
   mainPepper: { type: String, required: true },
-  imageUrl: { type: String, required: true },
+  imageUrl: { type: String },
   heat: { type: Number, required: true },
-  likes: { type: Number, required: true },
-  dislikes: { type: Number, required: true },
-  usersLiked: ["String <userId>"],
-  userDisliked: ["String <userId>"],
+  likes: { type: Number, required: true, default: 0 },
+  dislikes: { type: Number, required: true, default: 0 },
+  usersLiked: { type: [String], require: true, default: 0 },
+  userDisliked: { type: [String], require: true, default: 0 },
 });
 
 module.exports = mongoose.model("ChiliSauce", chiliSauceSchema);
