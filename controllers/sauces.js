3602d772257ee437df54f36a0a5e441e83c1785d
Maxime Ronjat <maxime.ronjat@gmail.com>
2023-03-31 14:39:38 +0200

    ajoute route et controller get one sauce

--- controllers/sauces.js
+++ controllers/sauces.js
@@ -55,7 +55,8 @@ exports.modifySauce = (req, res, next) => {
       res.status(400).json({ error });
     });
 };
-//fonction pour voir toute les sauces
+
+//fonction pour voir toutes les sauces
 exports.getAllSauces = (req, res, next) => {
   chiliSauce
     .find()
@@ -64,6 +65,22 @@ exports.getAllSauces = (req, res, next) => {
     })
     .catch((error) => {
       res.status(400).json({
+        error: error,
+      });
+    });
+};
+
+//fonction pour voir une sauce
+exports.getOneSauce = (req, res, next) => {
+  chiliSauce
+    .findOne({
+      _id: req.params.id,
+    })
+    .then((chiliSauce) => {
+      res.status(200).json(chiliSauce);
+    })
+    .catch((error) => {
+      res.status(404).json({
         error: error,
       });
     });
--- routes/sauces.js
+++ routes/sauces.js
@@ -3,10 +3,11 @@ const router = express.Router();
 const auth = require("../middleware/auth"); //pour vérifier l'authentification
 const multer = require("../middleware/multer-config"); //pour les images apres l'auth pour éviter les images non authentifier
 
-const ctrlChili = require("../controllers/sauces");
+const ctrlSauces = require("../controllers/sauces");
 
-router.post("/", auth, multer, ctrlChili.createSauce); // route pour la création de sauce
-router.put("/:id", auth, multer, ctrlChili.modifySauce); // route pour la modification d'une sauce
-router.get("/", auth, ctrlChili.getAllSauces);
+router.post("/", auth, multer, ctrlSauces.createSauce); // route pour la création de sauce
+router.put("/:id", auth, multer, ctrlSauces.modifySauce); // route pour la modification d'une sauce
+router.get("/", auth, ctrlSauces.getAllSauces); //fonction pour voir toutes les sauces
+router.get("/:id", auth, ctrlSauces.getOneSauce); // fonction pour voir une sauce
 
 module.exports = router;
