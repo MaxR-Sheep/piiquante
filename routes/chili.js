7dc68e8bec54b194f8ec0a3f220e19842f5a2399
Maxime Ronjat <maxime.ronjat@gmail.com>
2023-03-31 12:04:35 +0200

    ajout route sauce création

--- routes/chili.js
+++ routes/chili.js
@@ -2,3 +2,7 @@ const express = require("express");
 const router = express.Router();
 const auth = require("../middleware/auth");
 const multer = require("../middleware/multer-config");
+
+const ctrlChili = require("../controllers/chili");
+
+router.get("/", auth, ctrlChili.createSauce);
