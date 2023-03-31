2a4154e400cad3dc4d68018383aa1f38fa61f023
Maxime Ronjat <maxime.ronjat@gmail.com>
2023-03-31 12:04:05 +0200

    ajout création chili sauce

--- controllers/chili.js
+++ controllers/chili.js
@@ -1,1 +1,23 @@
 const chiliSauce = require("../models/ChiliSauce");
+
+exports.createSauce = (req, res, next) => {
+  const chiliSauceObject = JSON.parse(req.body.thing);
+  delete chiliSauceObject._id;
+  delete chiliSauceObject._userId;
+  const chiliSauce = new chiliSauce({
+    ...chiliSauceObject,
+    userId: req.auth.userId,
+    imageUrl: `${req.protocol}://${req.get("host")}/images/${
+      req.file.filename
+    }`,
+  });
+
+  thing
+    .save()
+    .then(() => {
+      res.status(201).json({ message: "Objet enregistré !" });
+    })
+    .catch((error) => {
+      res.status(400).json({ error });
+    });
+};
